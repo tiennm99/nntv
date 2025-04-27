4bc0b73c9a81dfba835d2c121d8b8e41047243df
tiennm99 <tiennm99@outlook.com>
2025-04-27 11:03:20 +0700

    feat: init again with phaser vite webbundler

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
-import { Menu } from './scenes/Menu.js';
-import { Game } from './scenes/Game.js';
+import StartGame from './game/main';
 
-const config = {
-    type: Phaser.AUTO,
-    title: 'nntv',
-    description: 'Night Ninja: Twilight Voyage',
-    parent: 'game-container',
-    width: 800,
-    height: 600,
-    pixelArt: false,
-    scene: [Menu, Game],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }
-        }
-    }
-}
-new Phaser.Game(config);
+document.addEventListener('DOMContentLoaded', () => {
+
+    StartGame('game-container');
+
+});
